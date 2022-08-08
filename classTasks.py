a4c27d6d486f9e55e6e0126a4ec3be5c3ab43fbd
Kirill <makarevich.mks@gmail.com>
2022-08-08 18:51:27 +0400

    Создал класс телефон и позвонил

--- classTasks.py
+++ classTasks.py
@@ -1,21 +1,30 @@
+import time
+
+
 class Phone:
 
     default_color = 'Grey'
     default_model = 'C385'
+    turn_on = False
 
     def __init__(self, color, model):
         # Динамические поля (переменные объекта)
         self.color = color
         self.model = model
 
-    def turn_on(self):
-        print("Iphone is alvie")
-        pass
-
     def call(self, number):
         print(f"calling {number}...")
         pass
+    def turn_on_phone(self):
+        if self.turn_on == False:
+            self.turn_on = True
+            print("Hello!")
+        else:
+            print("Phone is awake!")
+
 
 mobilephone = Phone("Blue", "Iphone")
-mobilephone.turn_on()
+mobilephone.turn_on_phone()
 mobilephone.call(89270187935)
+
+print(dir(mobilephone))
